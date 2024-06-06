c82b2bf6b01a2ebe51eac3b2155534d019d585c1
Lucas Fiorelli <fiorellilucas@gmail.com>
2024-06-06 07:45:18 -0300

    Receber request de leitura do sensor

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express" 
+import bodyParser from "body-parser"
 
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get("/", async (req, res) => {
   res.send("Hello world")
+})
+
+app.post("/leitura", async (req, res) => {
+  let leituraSensor = req.body
+  console.log(leituraSensor)
+  res.sendStatus(200)
 })
 
 app.listen(port, () => {
