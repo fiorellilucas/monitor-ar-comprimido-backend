bd3aeb6cc4a0b3fe062d6bb624b3c2e2ed0d348f
Lucas Fiorelli <fiorellilucas@gmail.com>
2024-06-06 08:55:10 -0300

    Salvar dados do sensor no arquivo csv

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 import express from "express" 
 import bodyParser from "body-parser"
+import fs, { write } from "node:fs"
 
 const app = express()
 const port = 3000
+
+const csvExiste = () => {
+  if (!fs.existsSync("./dados_arcomp.csv")) {
+    let linhaHeaders = []
+    let headers = ["data", "leitura", "\n"]
+    linhaHeaders.push(headers.join(","))
+
+    linhaHeaders = Buffer.from(linhaHeaders[0])
+
+    fs.writeFile("dados_arcomp.csv", linhaHeaders, (err) => {
+      if (err) throw (err)
+      console.log("Arquivo criado")
+    })
+  }
+  return true
+}
+
+const addDadosSensor = (leituraSensor) => {
+  if (csvExiste()) {
+    let data = new Date()
+    let leitura = leituraSensor["sensor"]
+
+    let linhaCsv = []
+    linhaCsv.push([data, leitura, "\n"].join(","))
+
+    linhaCsv = Buffer.from(linhaCsv[0])
+
+    fs.appendFile("dados_arcomp.csv", linhaCsv, (err) => {
+      if (err) throw (err)
+      console.log("Dados salvos")
+    })
+  }
+}
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -12,7 +46,9 @@ app.get("/", async (req, res) => {
 
 app.post("/leitura", async (req, res) => {
   let leituraSensor = req.body
-  console.log(leituraSensor)
+
+  addDadosSensor(leituraSensor)
+
   res.sendStatus(200)
 })
 
